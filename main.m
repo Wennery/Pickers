bb133d5417ec95b3a3aacfa76bc65a1d87ce3afe
Wennery <wenjianbest1@gmail.com>
2012-10-12 15:17:17 +0800

    third commit

---
+++ main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Pickers
+//
+//  Created by Wenjian Zhou on 10/10/12.
+//  Copyright (c) 2012 Wenjian Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+	//just for a github exercise
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WJAppDelegate class]));
+    }
+}
