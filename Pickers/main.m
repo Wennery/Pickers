1e10c5f2adc25f169d2d511fe98fca320af69422
Wennery <wenjianbest1@gmail.com>
2012-10-12 15:31:20 +0800

    second commit

--- Pickers/main.m
+++ Pickers/main.m
@@ -13,6 +13,7 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+	//just for a github exercise
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([WJAppDelegate class]));
     }
 }
